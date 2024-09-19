45a079aeef951d23e0ed082c80727e7c822696dd
DavidOHanlon85 <159072612+DavidOHanlon85@users.noreply.github.com>
2024-09-19 11:01:37 +0100

    Add files via upload

---
+++ Exercise01_LiteraryQuotations.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package labExercises;
+
+/**
+ * 
+ */
+public class Exercise01_LiteraryQuotations {
+	
+	//Methods
+	
+	/**
+	 * This method outputs a Shakespeare Quote
+	 */
+	public void shakespeareQuote() {
+		System.out.println("Good night, good night! Parting is such sweet sorrow, that I shall say good night till it be morrow. Shakespeare");
+	}
+	
+	/**
+	 * This method outputs an Oscar Wilde Quote
+	 */
+	public void oscarWildeQuote() {
+		System.out.println("Experience is simply the name we give out mistakes. Wilde");
+	}
+	
+	/**
+	 * This method outputs a Churchill Quote
+	 */
+	public void churchillQuote() {
+		System.out.println("Continuous effort - not strength or intelligence - is the key to unlocking out potential. Churchill");
+	}
+	
+	/**
+	 * This method outputs a George W.Bush Quote
+	 */
+	public void georgeWBushQuote() {
+		System.out.println("One of the great things about books is sometimes there are some fantastic pictures. George W. Bush");
+	}
+	
+	
+	/**
+	 * This method outputs an Albert Einstein Quote
+	 */
+	public void albertEinsteinQuote() {
+		System.out.println("If the facts don't fir the theory, change the facts. Albert Einstein");
+	}
+
+}
---
+++ Exercise01_TestDriver.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package labExercises;
+
+/**
+ * 
+ */
+public class Exercise01_TestDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Exercise01_LiteraryQuotations quotes = new Exercise01_LiteraryQuotations();
+		
+		quotes.churchillQuote();
+		
+		System.out.println();
+		
+		quotes.shakespeareQuote();
+		
+		System.out.println();
+		
+		quotes.oscarWildeQuote();
+		
+		System.out.println();
+		
+		quotes.georgeWBushQuote();
+		
+		System.out.println();
+
+		quotes.albertEinsteinQuote();
+
+	}
+
+}
